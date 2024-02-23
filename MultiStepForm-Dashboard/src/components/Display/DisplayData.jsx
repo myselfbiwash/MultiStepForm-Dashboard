f4f3f53ffd7949ba978eaf25ddfadaff54c46a25
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-02-23 15:06:47 +0545

    Refactor DisplayData component to conditionally render subjects with marks

--- MultiStepForm-Dashboard/src/components/Display/DisplayData.jsx
+++ MultiStepForm-Dashboard/src/components/Display/DisplayData.jsx
@@ -35,32 +35,42 @@ const DisplayData = () => {
                 <h2>Faculty Subjects</h2>
                 {data.facultySubjects &&
                     Object.entries(data.facultySubjects).map(
-                        ([faculty, semesters]) => (
-                            <div key={faculty}>
-                                <h3>{faculty}</h3>
-                                {Object.entries(semesters).map(([semester, subjects]) => (
-                                    <div key={semester}>
-                                        <h4>Semester {semester}</h4>
-                                        <table>
-                                            <thead>
-                                                <tr>
-                                                    <th>Subject</th>
-                                                    <th>Marks</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {subjects.map((subject, index) => (
-                                                    <tr key={index}>
-                                                        <td>{subject.name}</td>
-                                                        <td>{subject.marks}</td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                ))}
-                            </div>
-                        )
+                        ([faculty, semesters]) => {
+                            const hasMarks = Object.values(semesters).some(subjects =>
+                                subjects.some(subject => subject.marks !== null && subject.marks !== undefined)
+                            );
+                            return hasMarks ? (
+                                <div key={faculty}>
+                                    <h3>{faculty}</h3>
+                                    {Object.entries(semesters).map(([semester, subjects]) => {
+                                        const hasMarksInSemester = subjects.some(subject => subject.marks !== null && subject.marks !== undefined);
+                                        return hasMarksInSemester ? (
+                                            <div key={semester}>
+                                                <h4>Semester {semester}</h4>
+                                                <table>
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Subject</th>
+                                                            <th>Marks</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        {subjects.map((subject, index) =>
+                                                            subject.marks !== null && subject.marks !== undefined ? (
+                                                                <tr key={index}>
+                                                                    <td>{subject.name}</td>
+                                                                    <td>{subject.marks}</td>
+                                                                </tr>
+                                                            ) : null
+                                                        )}
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        ) : null;
+                                    })}
+                                </div>
+                            ) : null;
+                        }
                     )}
             </div>
         </div>
